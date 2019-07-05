54c74142f1eebcd5492d52b0a6a5b966cd32fae5
Dani Vicario <danigv.email@gmail.com>
2019-07-05 13:19:59 +0200

    added api key in .env

--- where-do-you-think/app.js
+++ where-do-you-think/app.js
@@ -62,6 +62,7 @@ hbs.registerHelper('ifUndefined', (value, options) => {
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.GOOGLEMAPSAPIKEY = process.env.GOOGLEMAPSAPIKEY;
 
 
 // Enable authentication using session + passport
